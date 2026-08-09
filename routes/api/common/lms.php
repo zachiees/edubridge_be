4b1f80371ef98cadec2fdccc7a48e288423d8e7c
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-08-09 09:42:25 +0800

    lms course find

--- routes/api/common/lms.php
+++ routes/api/common/lms.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('common/lms')->middleware(['auth:sanctum'])
     ->group(function () {
         Route::get('courses',               [Lms::class, 'courses']);
-        Route::get('courses/:id',           [Lms::class, 'course_find']);
+        Route::get('courses/{id}',           [Lms::class, 'course_find']);
         Route::get('course_categories',     [Lms::class, 'course_categories']);
         Route::get('roles',                 [Lms::class, 'roles']);
         Route::get('users_by_role',         [Lms::class, 'users_by_role']);
