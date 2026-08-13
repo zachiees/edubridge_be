d71c681540f60fe3912843d4476f85814cca2c7b
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-08-13 18:19:43 +0800

    evaluation respondents

---
+++ database/migrations/2026_08_13_101330_create_teacher_evaluation_respondents_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher_evaluation_respondents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('evaluation_id')
+                  ->constrained('teacher_evaluations')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate();
+            $table->foreignId('student_id')
+                  ->constrained('users')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate();
+            $table->foreignId('teacher_id')
+                  ->constrained('users')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate();
+            $table->foreignId('course_id')
+                  ->constrained('courses')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate();
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher_evaluation_respondents');
+    }
+};
