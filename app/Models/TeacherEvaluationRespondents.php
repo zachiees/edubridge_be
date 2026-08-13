a6b2f55a99fee9aa3e99f2935b71cedac697066b
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-08-13 18:33:46 +0800

    model update

--- app/Models/TeacherEvaluationRespondents.php
+++ app/Models/TeacherEvaluationRespondents.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class TeacherEvaluationRespondents extends Model
 {
     //
+    protected $hidden = ['id'];
+    protected $fillable = [
+        'evaluation_id',
+        'student_id',
+        'teacher_id',
+        'course_id'
+    ];
+
 }
