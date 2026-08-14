46d0c470fa53d2f69eee826265740e74bd0b6431
Zach Madrilejos <zach.madrilejos@gmail.com>
2026-08-14 13:20:24 +0800

    eval response table

--- app/Models/TeacherEvaluationResponse.php
+++ app/Models/TeacherEvaluationResponse.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class TeacherEvaluationResponse extends Model
 {
     //
+    protected $hidden = [
+        'id',
+        'evaluation_id',
+        'respondent_id',
+        'question_id',
+    ];
+
+    protected $fillable = [
+        'evaluation_id',
+        'respondent_id',
+        'question_id',
+        'rating',
+        'response'
+    ];
+
+
+
 }
